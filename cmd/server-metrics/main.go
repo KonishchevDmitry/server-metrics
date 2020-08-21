85b100b2b8e628968e8b303afc2df00a3f49f387
Dmitry Konishchev <konishchev@gmail.com>
2020-08-21 12:03:39 +0300

    Service stub

--- cmd/server-metrics/main.go
+++ cmd/server-metrics/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	_ "github.com/prometheus/client_golang/prometheus"
+	//_ "github.com/prometheus/client_golang/prometheus/promhttp"
+	//_ "github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+/*
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+func recordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+*/
 
 func main() {
+	//if false {
+	//	recordMetrics()
+	//
+	//	http.Handle("/metrics", promhttp.Handler())
+	//	http.ListenAndServe(":2112", nil)
+	//}
+
 	fmt.Println("It works!")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/KonishchevDmitry/server-metrics
 
 go 1.14
+
+require github.com/prometheus/client_golang v1.7.1
