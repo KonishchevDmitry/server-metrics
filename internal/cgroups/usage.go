7e7461a87f809939028c6577980b7059d9155bef
Dmitry Konishchev <konishchev@gmail.com>
2021-10-20 09:52:23 +0300

    Introduce usage module

--- internal/cgroups/usage.go
+++ internal/cgroups/usage.go
@@ -5,19 +5,15 @@ type ToNamedUsage interface {
 }
 
 type NamedUsage struct {
-	Name  string
-	Value *int64
-
-	Monotonic    bool
-	AllowedError int64
+	Name      string
+	Value     *int64
+	Precision int64
 }
 
-func MakeMonotonicNamedUsage(name string, value *int64, allowedError int64) NamedUsage {
+func MakeNamedUsage(name string, value *int64, precision int64) NamedUsage {
 	return NamedUsage{
-		Name:  name,
-		Value: value,
-
-		Monotonic:    true,
-		AllowedError: allowedError,
+		Name:      name,
+		Value:     value,
+		Precision: precision,
 	}
 }
