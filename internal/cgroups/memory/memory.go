c8a55726206ed1af34d9c3d906292d961fe08a51
Dmitry Konishchev <konishchev@gmail.com>
2022-07-08 22:03:19 +0300

    Account swap usage

--- internal/cgroups/memory/memory.go
+++ internal/cgroups/memory/memory.go
@@ -3,6 +3,9 @@ package memory
 import (
 	"context"
 	"fmt"
+	"io"
+	"strconv"
+	"strings"
 
 	"github.com/pkg/math"
 
@@ -13,6 +16,7 @@ import (
 )
 
 var rssMetric = metrics.ServiceMetric("memory", "rss", "Anonymous and swap cache memory usage.")
+var swapMetric = metrics.ServiceMetric("memory", "swap", "Non-cached swap usage.")
 var cacheMetric = metrics.ServiceMetric("memory", "cache", "Page cache memory usage.")
 var kernelMetric = metrics.ServiceMetric("memory", "kernel", "Kernel data structures.")
 
@@ -70,6 +74,20 @@ func (c *Collector) collect(group *cgroups.Group) (Usage, bool, error) {
 		return Usage{}, exists, err
 	}
 
+	var swap int64
+	if !group.IsRoot() {
+		if exists, err := group.ReadProperty("memory.swap.current", func(file io.Reader) error {
+			data, err := io.ReadAll(file)
+			if err == nil {
+				value := strings.TrimSpace(string(data))
+				swap, err = strconv.ParseInt(value, 10, 64)
+			}
+			return err
+		}); err != nil || !exists {
+			return Usage{}, exists, err
+		}
+	}
+
 	var keyErr error
 	get := func(name string) int64 {
 		value, err := stat.Get(name)
@@ -81,6 +99,7 @@ func (c *Collector) collect(group *cgroups.Group) (Usage, bool, error) {
 
 	usage := Usage{
 		rss:    get("anon"),
+		swap:   math.MaxInt64(0, swap-get("swapcached")),
 		cache:  get("file"),
 		kernel: get("kernel_stack") + get("pagetables") + get("percpu") + get("slab_unreclaimable") + get("sock"),
 	}
@@ -115,16 +134,20 @@ func (c *Collector) collectRoot(usage Usage, children []*cgroups.Group, isOption
 }
 
 func (c *Collector) record(ctx context.Context, service string, usage Usage) {
-	logging.L(ctx).Debugf("* %s: memory: rss=%d, cache=%d, kernel=%d", service, usage.rss, usage.cache, usage.kernel)
+	logging.L(ctx).Debugf(
+		"* %s: memory: rss=%d, swap=%d, cache=%d, kernel=%d",
+		service, usage.rss, usage.swap, usage.cache, usage.kernel)
 
 	labels := metrics.ServiceLabels(service)
 	rssMetric.With(labels).Set(float64(usage.rss))
+	swapMetric.With(labels).Set(float64(usage.swap))
 	cacheMetric.With(labels).Set(float64(usage.cache))
 	kernelMetric.With(labels).Set(float64(usage.kernel))
 }
 
 type Usage struct {
 	rss    int64
+	swap   int64
 	cache  int64
 	kernel int64
 }
@@ -134,6 +157,7 @@ var _ cgroups.ToUsage = &Usage{}
 func (u *Usage) ToUsage() []cgroups.Usage {
 	return []cgroups.Usage{
 		cgroups.MakeUsage("rss memory usage", &u.rss),
+		cgroups.MakeUsage("non-cached swap usage", &u.swap),
 		cgroups.MakeUsage("page cache usage", &u.cache),
 		cgroups.MakeUsage("kernel memory usage", &u.kernel),
 	}
